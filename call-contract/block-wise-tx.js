d0203144857083d30e97918f69e8c961368373c8
usamarajpoot03 <usamarajpoot03@yahoo.com>
2021-05-27 02:28:05 +0500

    block wise tx

--- call-contract/block-wise-tx.js
+++ call-contract/block-wise-tx.js
@@ -1,25 +1,24 @@
-// REQUIRED MODULES
 const path = require("path");
 require("dotenv").config({
   path: "./.env",
 });
-
 const web3 = require("web3");
 const Tx = require("ethereumjs-tx").Transaction;
-const cron = require("node-cron");
-
-// ABIs
 const testContract = require("./abis/test-contract");
 
-// Infura HttpProvider Endpoint
 web3js = new web3(
-  new web3.providers.HttpProvider(
-    "https://ropsten.infura.io/v3/dedac6824bfa4dd5833903d6efa48af8"
+  new web3.providers.WebsocketProvider(
+    "wss://ropsten.infura.io/ws/v3/dedac6824bfa4dd5833903d6efa48af8"
   )
 );
 
-let counter = 1;
+let isSubscribed = false;
+let transactionMinedBlockNumber = null;
+const blockDiff = 10;
+let isTransactionInProgress = false;
+
 const runTransaction = () => {
+  console.log("Transaction Initiated");
   var myAddress = process.env.MY_PUBLIC_ADDRESS;
   var privateKey = Buffer.from(process.env.MY_PRIVATE_KEY, "hex");
   //contract abi is the array that you can get from the ethereum wallet or etherscan
@@ -30,7 +29,7 @@ const runTransaction = () => {
   var count;
   // get transaction count, later will used as nonce
   web3js.eth.getTransactionCount(myAddress).then(function (v) {
-    console.log("Count: " + v);
+    // console.log("Count: " + v);
     count = v;
 
     //creating raw tranaction
@@ -40,10 +39,10 @@ const runTransaction = () => {
       gasLimit: web3js.utils.toHex(210000),
       to: contractAddress,
       value: "0x0",
-      data: contract.methods.setName("FROM NODE - " + counter).encodeABI(),
+      data: contract.methods.setName("FROM NODE").encodeABI(),
       nonce: web3js.utils.toHex(count),
     };
-    console.log(rawTransaction);
+    // console.log(rawTransaction);
     //creating tranaction via ethereumjs-tx
     var transaction = new Tx(rawTransaction, {
       chain: "ropsten",
@@ -54,14 +53,31 @@ const runTransaction = () => {
     //   sending transacton via web3js module
     web3js.eth
       .sendSignedTransaction("0x" + transaction.serialize().toString("hex"))
-      .on("transactionHash", (res) => {
-        console.log(res);
-        counter++;
+      .on("receipt", (res) => {
+        transactionMinedBlockNumber = res.blockNumber;
+        console.log("New Transaction Mined : " + transactionMinedBlockNumber);
+        isTransactionInProgress = false;
+        if (!isSubscribed) {
+          isSubscribed = true;
+          web3js.eth.subscribe("newBlockHeaders", (err, res) => {
+            const { number: currentBlockMined } = res;
+            console.log(
+              "Currently Mined Block : " +
+                currentBlockMined +
+                " Previous Transaction Mined in : " +
+                transactionMinedBlockNumber
+            );
+            if (
+              !isTransactionInProgress &&
+              transactionMinedBlockNumber + blockDiff <= currentBlockMined
+            ) {
+              isTransactionInProgress = true;
+              runTransaction();
+            }
+          });
+        }
       });
   });
 };
 
-cron.schedule("*/15 * * * *", function () {
-  console.log("Transaction Started - " + counter);
-  runTransaction();
-});
+runTransaction();
